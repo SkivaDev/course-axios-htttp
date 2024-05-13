9aa81baa927f2c074b3bfca7433af7f7ac756940
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-05-13 12:05:33 -0500

    feat: :sparkles: GET AXIOS

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 
 const getUsers = async () => {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/users')
+    const response = await axios.get('https://jsonplaceholder.typicode.com/users', {
+        params: {
+            _limit: 2
+        }
+    })
     console.log(response);
 }
 
